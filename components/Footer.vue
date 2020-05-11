327d343e2330a20502802d4c534f6baa35990828
Kamil Sztefko <sztefkokamil@gmail.com>
2020-05-11 16:16:02 +0200

    Add Footer Content
    Added template.
    Added styles.

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,3 +1,47 @@
 <template>
-  <div></div>
+  <footer class="footer">
+    <div class="inner-footer">
+      <span>Wood-Stairs 2020 &copy; All Rights Reserved</span>
+      <span>Design & implementation kontakt@kamilsztefko.pl</span>
+    </div>
+  </footer>
 </template>
+
+<style lang="scss">
+.footer {
+  margin: 70px 0 0;
+  font-size: 14px;
+  background: var(--menu-bg);
+  padding: 30px 0;
+
+  .inner-footer {
+    max-width: 840px;
+    margin: 0 auto;
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    padding: 0 30px;
+
+    span:first-child {
+      margin: 0 0 10px;
+    }
+  }
+
+  @media screen and (min-width: 700px) {
+    .inner-footer {
+      flex-flow: row;
+      justify-content: space-between;
+
+      span:first-child {
+        margin: 0;
+      }
+    }
+  }
+
+  @media screen and (min-width: 900px) {
+    .inner-footer {
+      padding: 0;
+    }
+  }
+}
+</style>
