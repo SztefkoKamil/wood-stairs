7c91b5de1fb12cc08266960d12d60591351e044b
Kamil Sztefko <sztefkokamil@gmail.com>
2020-05-11 12:48:11 +0200

    Add About content
    Added text content.
    Added image.
    Added styles.

--- components/About.vue
+++ components/About.vue
@@ -1,3 +1,42 @@
 <template>
-  <div></div>
+  <article id="about" class="about-container">
+    <div>
+      <h2 class="section-header">Who we Are</h2>
+      <p>
+        Culpa culpa duis in et deserunt proident exercitation velit do magna
+        laborum. Veniam excepteur voluptate aliqua sunt aute eu cillum qui
+        exercitation eiusmod mollit proident. Enim amet reprehenderit quis
+        aliquip laborum esse deserunt. Commodo id labore est ad nisi occaecat
+        sint incididunt pariatur eiusmod.
+      </p>
+      <img
+        class="shadow-box"
+        src="../assets/images/thumbnail-about.webp"
+        alt="wooden stairs"
+      />
+      <p>
+        In Lorem ex est proident eiusmod id eu tempor. Minim officia sit
+        incididunt enim ea labore aliqua enim amet velit nulla ex. Dolore ad
+        laboris deserunt elit irure culpa tempor aliquip incididunt dolore
+        proident laboris enim. Reprehenderit quis adipisicing aliquip cupidatat
+        et sint laborum fugiat cupidatat excepteur occaecat in elit. Labore ad
+        pariatur mollit est velit sunt. Elit tempor adipisicing proident nostrud
+        laborum Lorem ullamco. Officia ea in pariatur ullamco adipisicing sunt
+        nostrud magna proident nostrud. Ullamco cupidatat laborum commodo aliqua
+        sint exercitation minim ullamco ad.
+      </p>
+    </div>
+  </article>
 </template>
+
+<style lang="scss">
+.about-container {
+  padding: 70px 30px 0;
+
+  img {
+    width: 100%;
+    border-radius: 50px;
+    margin: 30px 0;
+  }
+}
+</style>
