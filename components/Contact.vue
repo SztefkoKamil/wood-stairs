62be9815ac5d3ecdc43ccab160ee831e65c7b1b9
Kamil Sztefko <sztefkokamil@gmail.com>
2020-05-11 16:15:38 +0200

    Add Contect content
    Added address SVG icons.
    Add template.
    Add iframe with map.
    Add styles.

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,3 +1,62 @@
 <template>
-  <div></div>
+  <article id="contact" class="contact-container">
+    <h2 class="section-header">Contact Us</h2>
+    <div>
+      <div class="address">
+        <div>
+          <img src="../assets/images/icon-map.svg" alt="" />
+          <h6>
+            6405 Thornridge Cir undefined Jacksonville, Hawaii 80417 United
+            States
+          </h6>
+        </div>
+        <div>
+          <img src="../assets/images/icon-phone.svg" alt="" />
+          <h6>(907) 555-0101</h6>
+        </div>
+        <div>
+          <img src="../assets/images/icon-mail.svg" alt="" />
+          <h6>contact@wood-stairs.com</h6>
+        </div>
+      </div>
+      <iframe
+        frameborder="0"
+        scrolling="no"
+        src="https://www.openstreetmap.org/export/embed.html?bbox=-118.85614871978761%2C34.15663346692696%2C-118.82576465606691%2C34.187594858235826&amp;layer=mapnik&amp;marker=34.172115582379185%2C-118.84095668792725"
+      ></iframe>
+    </div>
+  </article>
 </template>
+
+<style lang="scss">
+.contact-container {
+  padding: 70px 30px 0;
+
+  .address {
+    & > div {
+      display: flex;
+      align-items: center;
+      margin: 0 0 30px;
+
+      &:last-child {
+        margin: 0 0 50px;
+      }
+    }
+
+    img {
+      width: 50px;
+    }
+
+    h6 {
+      font-weight: 400;
+      font-size: 18px;
+      margin: 0 0 0 20px;
+    }
+  }
+
+  iframe {
+    width: 100%;
+    height: 60vw;
+  }
+}
+</style>
