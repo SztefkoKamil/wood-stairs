5b80c8771a260729772ffad518099b200ccd9843
Kamil Sztefko <sztefkokamil@gmail.com>
2020-05-12 08:50:32 +0200

    Add RWD desktop styles for About

--- components/About.vue
+++ components/About.vue
@@ -1,19 +1,27 @@
 <template>
   <article id="about" class="about-container">
     <div>
-      <h2 class="section-header">Who we Are</h2>
-      <p>
-        Culpa culpa duis in et deserunt proident exercitation velit do magna
-        laborum. Veniam excepteur voluptate aliqua sunt aute eu cillum qui
-        exercitation eiusmod mollit proident. Enim amet reprehenderit quis
-        aliquip laborum esse deserunt. Commodo id labore est ad nisi occaecat
-        sint incididunt pariatur eiusmod.
-      </p>
-      <img
-        class="shadow-box"
-        src="../assets/images/thumbnail-about.webp"
-        alt="wooden stairs"
-      />
+      <h2 class="section-header">Who we are</h2>
+      <div class="inner">
+        <p>
+          Culpa culpa duis in et deserunt proident exercitation velit do magna
+          laborum. Veniam excepteur voluptate aliqua sunt aute eu cillum qui
+          exercitation eiusmod mollit proident. Enim amet reprehenderit quis
+          aliquip laborum esse deserunt. Commodo id labore est ad nisi occaecat
+          sint incididunt pariatur eiusmod.
+        </p>
+        <picture>
+          <source
+            media="(max-width: 768px)"
+            srcset="../assets/images/thumbnail-about.webp"
+          />
+          <img
+            class="shadow-box"
+            src="../assets/images/thumbnail-about-square.webp"
+            alt="wooden stairs"
+          />
+        </picture>
+      </div>
       <p>
         In Lorem ex est proident eiusmod id eu tempor. Minim officia sit
         incididunt enim ea labore aliqua enim amet velit nulla ex. Dolore ad
@@ -37,6 +45,33 @@
     width: 100%;
     border-radius: 50px;
     margin: 30px 0;
+  }
+
+  @media screen and (min-width: 768px) {
+    p {
+      font-size: 20px;
+    }
+
+    .inner {
+      display: flex;
+      align-items: flex-end;
+      margin: 0 0 30px;
+
+      p {
+        order: 2;
+        width: 66%;
+        padding: 0 0 20px 30px;
+      }
+
+      picture {
+        width: 33%;
+      }
+
+      img {
+        border-radius: 50%;
+        margin: 0;
+      }
+    }
   }
 }
 </style>
