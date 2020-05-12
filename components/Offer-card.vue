a172abf162d70791908707fb472ba3002730c808
Kamil Sztefko <sztefkokamil@gmail.com>
2020-05-12 09:33:17 +0200

    Add RWD desktop styles for Offer-Card

--- components/Offer-card.vue
+++ components/Offer-card.vue
@@ -14,34 +14,74 @@ export default {
 <style lang="scss">
 .card-container {
   background: var(--card-bg);
-  margin: 0 0 50px;
+  margin: 0 auto 50px;
   border-top-left-radius: 50px;
+  border-top-right-radius: 50px;
   border-bottom-right-radius: 50px;
-  overflow: hidden;
+  max-width: 400px;
 
   &:last-child {
-    margin: 0;
+    margin-bottom: 0;
   }
 
   img {
     width: 100%;
+    border-top-left-radius: 50px;
     border-bottom-right-radius: 50px;
+    order: 2;
   }
 
   p {
     color: var(--font);
     padding: 20px 10px 30px;
   }
-}
 
-.reverse {
-  border-radius: 0;
-  border-top-right-radius: 50px;
-  border-bottom-left-radius: 50px;
-
-  img {
-    border-radius: 0;
+  &.reverse {
+    border-bottom-right-radius: 0;
+    border-top-right-radius: 50px;
     border-bottom-left-radius: 50px;
+
+    img {
+      border-bottom-right-radius: 0;
+      border-bottom-left-radius: 50px;
+      border-top-right-radius: 50px;
+      order: 0;
+    }
+  }
+
+  @media screen and (min-width: 768px) {
+    & {
+      display: flex;
+      align-items: flex-end;
+      max-width: 100%;
+      height: 140px;
+      margin-top: 145px;
+
+      p {
+        padding: 30px 20px 20px;
+      }
+
+      img {
+        border-top-right-radius: 50px;
+        width: 300px;
+      }
+    }
+
+    &.reverse {
+      margin-top: 120px;
+
+      img {
+        border-top-left-radius: 50px;
+      }
+    }
+  }
+
+  @media screen and (min-width: 910px) {
+    & {
+      p {
+        font-size: 18px;
+      }
+    }
   }
 }
 </style>
