3ffbc13a52797e88961eddbcd0501603825c09cb
Kamil Sztefko <sztefkokamil@gmail.com>
2020-05-14 16:54:53 +0200

    Add error page

---
+++ layouts/error.vue
@@ -0,0 +1,50 @@
+<template>
+  <article class="error-container">
+    <h1>{{ error.message }}</h1>
+    <h2>Error status: {{ error.statusCode }}</h2>
+    <n-link to="/">go back to Home Page</n-link>
+  </article>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  layout: '404'
+}
+</script>
+
+<style lang="scss">
+.error-container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+
+  h1 {
+    font-family: 'Volkhov';
+    font-size: 40px;
+    font-weight: 400;
+    text-align: center;
+    line-height: 130%;
+  }
+
+  h2 {
+    font-size: 30px;
+    font-weight: 400;
+    margin: 20px 0 50px;
+    text-align: center;
+  }
+
+  a {
+    text-decoration: none;
+    font-size: 20px;
+    color: var(--color-primary);
+    padding: 20px 30px;
+    border: 1px solid var(--primary);
+    background-color: var(--bg-secondary);
+  }
+}
+</style>
